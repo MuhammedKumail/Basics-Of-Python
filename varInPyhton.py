d97d0b22529cfaf6de84d49bc7a0c3478a22f4ea
MuhammedKumail <67711536+MuhammedKumail@users.noreply.github.com>
2022-03-23 18:29:44 +0500

    Create varInPyhton.py

---
+++ varInPyhton.py
@@ -0,0 +1,19 @@
+#
+
+# a few basic examples of immutable data types (may have same variables but have different data allocations):
+# numeeric data types (integer, float, etc.): 
+x = 2
+x = 2.0
+
+# string
+str1 = "I am using Python"
+
+# tuples (like lists but with different brackets):
+tup = (1, 2, 3, "hello")
+
+# a few basic examples of mutable data type (values can be updated at will and will have same memory allocaiton):
+# lists
+list1 = [1, 2, 3, 'hello']
+
+# dictionaries
+dict1 = {1: 'John', 2: 'Alice'}
